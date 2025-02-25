548bab86f44cba4aecba8ba0cccb015f57499cab
Максим Процюк <protsuk26@gmail.com>
2025-02-25 18:00:56 +0200

    Доробив 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
 
 const refs = {
     form : document.querySelector(".feedback-form"),
@@ -12,7 +12,23 @@ const refs = {
 refs.form.addEventListener("submit", onFormSubmit);
 refs.form.addEventListener("input", throttle(onAllFieldInput, 500));
 
-function onFormSubmit(){
 
+function onFormSubmit(evt){
+    evt.preventDefault()
+    if(refs.email.value === "" || refs.textarea.value === ""){
+        return alert("Всі поля мають бути заповнені")
+    }
+    console.log(formData)
+    localStorage.removeItem(STORAGE_KEY)
+    evt.target.reset()
+    formData = {}
 }
+
+function onAllFieldInput(evt){
+    formData[evt.target.name] = evt.target.value
+    localStorage.setItem(STORAGE_KEY , JSON.stringify(formData))
+}
+
+refs.email.value = formData.email ?? ""
+refs.textarea.value = formData.message ?? ""
 
